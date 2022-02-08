7961cabc5199a3896eede0e0f56705d7c1e3b115
ayanhalder194 <ayanhalder194@gmail.com>
2022-02-09 00:17:03 +0530

    created the updated copy

---
+++ pop-up_admin.js
@@ -0,0 +1,68 @@
+
+var btn = document.getElementById("myBtn");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="asrafali.jpg";
+    document.getElementById("admin-text").innerHTML="The ambitious man, a person with clear aspiration Capt. Asraf was introduced to the club in 2010 for the very first time. Handed over the role of Managing Director of Five Star Group of Companies, it was clear that his love and passion for football opted towards Southern Samity. His impeccable and undeniable contribution to the club has reached a certain place, he wants to be taken further at the ultimate title."
+}
+
+var btn = document.getElementById("myBtn1");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="Xlalit.jpeg"; 
+    document.getElementById("admin-text").innerHTML="Lalit Sharma, Vice President of Southern Samity, is an entrepreneur and former martial arts enthusiast. He received a Black belt in Karate while in school at the age of fourteen. He finished his Diploma in Mechanical Engineering. After years of success as the founder of an advertising company, he became interested in Southern Samity to embark on a new journey and to avoid the trappings of a business, intending to wear a track suit over a business suit here and there. <br/> For a man who has generally stayed out of spotlight, he made a name for himself in Karate championships in his early years. He was always involved in sports and won many district and state level championships in his student life. In 2005, Lalit turned to advertising. He was successfully able to launch his business career and expand the company’s operations. He brought the company to the current position through years of occupation and business acumen. <br/>  Southern Samity football club for him is a way to reconnect with his childhood path. He has high hopes that through the club’s joint efforts for the best infrastructure and the talent of players, the club has a promising future and will also act as a catalyst in making Indian football thrive. Whatever the future brings for the club, Lalit believes that there will likely be many interesting adventures that all will be a part of together."  ; 
+}
+
+var btn = document.getElementById("myBtn2");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="JiniaRoyChowdhury2.jpeg";
+    document.getElementById("admin-text").innerHTML="Jinia Roy Chowdhury started her career in the field of acting and modeling and for her, football was a natural passion. In 2018, when she became involved with the prestigious Southern Samity Football Club, she started making inroads into the world of football. She is a member of the Indian Football Association and is seeking the best way to develop women's football in West Bengal. Mentoring infrastructure, talent scouting, proper training, and coaching should be followed following a solid roadmap.";
+}
+
+var btn = document.getElementById("myBtn3");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="souravpal.jpg";
+    document.getElementById("admin-text").innerHTML="With an immense personality and desire to become a footballer, he then joined Southern Samity. He handled the duties of General Secretary day-in and day-out for many years. Many players who today play and play football in Bengal as well as in India are of Southern Samity descent. It is unquestionable that Mr. Sourav Pal loved his people and stood by them.";
+}
+
+var btn = document.getElementById("myBtn4");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="kushalban.jpg";
+    document.getElementById("admin-text").innerHTML="The association between Kushal Banerjee and football dates back to 1979 when he joined Kalighat Milani Football Club. He played for Calcutta Customs Club from 1980 to 1998, as well as a season for Rajasthan Club in 1991. His first official position was as an assistant coach for his home club from 1998 to 2000. Since then, he has held a number of administrative posts at his home club, eventually retiring as the Football secretary in early 2021 along with his professional retirement as the Superintendent of Customs. With his unbridled love for the game, he pledges that he will bring Southern Samity to new heights and provide a platform for young, raw talent.";
+}
+var btn = document.getElementById("myBtn5");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="amitsen2.jpg";
+    document.getElementById("admin-text").innerHTML="Mr. Amit Sen is the chief executive officer (CEO) of Southern Samity Football Club. He joined Southern Samity in 2010, pursuing an interest in football that started at an early age. He has expertise in Academy Accreditation, Club Licensing Criteria, Central Registration System, and Competition Management System. In 2012, he served as the Team Manager of Southern Samity in many All India Football Tournaments like the CFL Kolkata Premier Division Football League, 2nd Division I- League, etc. In 2013, he became the FIFA TMS Manager, which was a tremendous achievement.";
+}
+
+var btn = document.getElementById("myBtn6");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="rajdeepdutta.jpg";
+    document.getElementById("admin-text").innerHTML="Rajdeep Dutta has nearly ten years of experience as a football development officer working with young players. He is a sports fanatic with an affinity for football. He has also been associated with the Bidhannagar Municipal Sports Academy for the past five years. n the Academy, he managed the Junior, Elite and Sub Junior Teams for the 2018-2019 season. Additionally, he has served as an official for Calcutta Customs Club in the Premier A Division of the Calcutta Football League and other tournaments. During his tenure, he has visited the academies of Manchester United, Borussia Dortmund and Monaco F.C. to understand their development process first hand. Additionally, he has focused on Coach education as a critical aspect in the development of a young player.";
+}
+
+var btn = document.getElementById("myBtn7");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="TBagchi.jpg";
+    document.getElementById("admin-text").innerHTML=" Dr. Tirthankar Bagchi, professionally a Clinician, with 25 years of commendable experience in Healthcare and Clinical Services and currently gracing the position of the General Manager Medical Services & Medical Superintendent, AMRI Hospital, Kolkata has become an integral and inseparable part of Southern Samity Club as a life member within a short period of time. In tune with his professional commitments that bestows on him the responsibility of serving humanity, in his personal domain also, Dr. Bagchi indulges in various philanthropic activities which deserve accolades. <br> His association with the club is expected to add value to its mission and vision and will surely facilitate generous altruistic activities that our club has, in its ethos and culture.";
+}
+
+var btn = document.getElementById("myBtn8");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="PKMukherjee.jpeg";
+    document.getElementById("admin-text").innerHTML=" Dr. Pranab Kumar Mukherjee, the Senior Vice President of Southern Samity FC is well known physician in the city. And on the the other hand he is an Ex - Sports person who has left a glorious mark on the pages of the sport's history. Now he is looking guiding us with his administrative skills and experiences gained through the years.";
+}
+
+
+var btn = document.getElementById("popup-close");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "none";
+}
---
+++ popup_chief_coach.js
@@ -0,0 +1,70 @@
+
+var btn = document.getElementById("myBtn");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="eminentpersons-coach1.jpg";
+    document.getElementById("admin-text").innerHTML="Inivalappil Mani Vijayan is known as  KALO HARIN.IM Vijayan is an Arjuna award winner.He was a former professional Indian football player, who was born on 25th April 1969 in Thrissur, Kerala. <br/> #KEYfactor - Vijayan made his international debut for the Indian national team in 1989. He represented India in tournaments such as the Nehru Cup, SAAF Cup, SAF Games, pre-Olympics, and pre-World Cup. <br/> Past Experience – He has made remarkable contributions to Mohun Bagan, East Bengal, Churchill Brothers, JCT."
+}
+
+var btn = document.getElementById("myBtn1");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="eminentpersons-coach2.jpg"; 
+    document.getElementById("admin-text").innerHTML="In 2011-2012, he became chief coach of the Southern Samity Club, which achieved a great deal of success under his guidance. The Brazilian with his vast experience and expertise pulling up to making a renowned club. <br/> #KEYfactor – 5 decades of experience across the Middle East, Brazil, and more than 10 years in the Mecca of Indian Football. <br/> Past Experience – Beginning his coaching career in 1970 with Brazil's Madumira Football Club. He came to Asia and has coached leading outfits of Qatar and the United Arab Emirates. Gaining experience from there he served for Singapore National Team, East Bengal FC, and many more.";
+}
+
+var btn = document.getElementById("myBtn2");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="eminentpersons-coach3.jpg";
+    document.getElementById("admin-text").innerHTML="Shabbir Ali, (born 26 January 1956) is an Indian football manager and former player.He was awarded the Dhyan Chand Award, the highest award in Indian sports for lifetime achievement, given by Government of India in 2011.He is the first footballer to be named for the Dhyan Chand Award. As a footballer, Shabbir Ali was rated as the best player in India during the 1970s and 1980s. Arguably the finest striker of his time, Shabbir was a prolific goal scorer both at the national and international level. He rose to fame at a very early age when he captained India to win the Asian Youth championships in Bangkok jointly with Iran in 1974, a performance which even earned praise from the Prime Minister, Indira Gandhi. <br/> After playing with Tata Sports Club in Bombay for a few years, Shabbir Ali was lured away by the top Calcutta club, East Bengal in the late seventies. Later he joined Mohammedan Sporting and took the club to a great height before retiring from the same club in the mid-1980s.He played 72 international matches for India and scored 23 goals from 1972 to 1984. Mr. Shabbir Ali joined Southern Samity Club as a Chief Coach in the year 2012-2013.";
+}
+
+var btn = document.getElementById("myBtn3");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="eminentpersons-coach4.jpg";
+    document.getElementById("admin-text").innerHTML="Subrata Bhattacharya played for 17 years in Mohun Bagan (1974–1990). He became the Captain of Mohun Bagan in 1977; Mohun Bagan won the triple crown that season and became the first club to do so in Indian football history. He also led the side against a star-studded Cosmos team which had living legend Pele; both the teams shared honours as the match ended 2–2. <br/> Subrata Bhattacharya is an Indian former footballer who played as a defender for the Indian national team and spent 17 years with Mohun Bagan as a footballer. He won Indian National Football League for two times as a coach of Mohun Bagan. He has also managed East Bengal, Southern Samity, Prayag United as a coach. He was awarded the Arjuna Award in Football in 2019.";
+}
+
+var btn = document.getElementById("myBtn4");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="eminentpersons-coach5.jpg";
+    document.getElementById("admin-text").innerHTML="He is a talented Indian Coach. He has joined Southern Samity as a chief coach in the year 2014-15.";
+}
+var btn = document.getElementById("myBtn5");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="eminentpersons-coach6.jpg";
+    document.getElementById("admin-text").innerHTML="He is a renowned name in Kolkata's football as a coach . He has Joined Souhern  Samity Club as Chief Coach in the year 2015-16";
+}
+var btn = document.getElementById("myBtn6");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="eminentpersons-coach7.jpg";
+    document.getElementById("admin-text").innerHTML="Hemanta Dora (born 4TH December 1974) is a former Indian International football player who played as a goalkeeper. Dora has played for Mohun Bagan and won the 1997-98 National Football League. He played for Indian national team including the Nehru Cup in 1997 and the 1998 Bangkok Asian Games. Mr. Hemanta Dora took charge as a chief coach of our club Southern Samity, during the period  from 2016 TO 2019 football session.";
+}
+var btn = document.getElementById("myBtn7");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="eminentpersons-coach10.jpg";
+    document.getElementById("admin-text").innerHTML="Mr. Utpal Mukherjee took his charge as youth development coach from 2014. He joined southern samity club as an asst. Coaching staff and till today he is our youth development coach.";
+}
+var btn = document.getElementById("myBtn8");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="eminentpersons-coach9.jpg";
+    document.getElementById("admin-text").innerHTML="Mr. SNEHASHISH CHAKRABORTY Played for Jadavpur University from 1983 to 1986. Youth India in the year 1986 held at Riyadh. Then join Aryan Club in the year 1987 played upto 1988. In the year 1989 join for Mohun Bagan. 1990 East Bengal and achieve triple crown. Santosh Trophy 1990 held at GOA. Again back to Aryan Club. Again in the year 1995 signed for Mohun Bagan. His favorite coach was Sir Achyut Banerjee, Sir Arun Singha, Syeed Nayeemuddin, Sir Sushil. Bhattacharya, Sir Amal Dutta. Mr. Snehashish Chakraborty joined Southern Samity Club as an Asst. & Goal Keeper Coach from 2013 to till today.";
+}
+var btn = document.getElementById("myBtn9");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "flex";
+    document.getElementById("admin-image").src="eminentpersons-coach8.jpg";
+    document.getElementById("admin-text").innerHTML="The Great Player Mr. Mehetab Hossain Chief Coach of our Club Southern Samity season 2019-2020. Mehtab Hossain (born 5 September 1985) is a retired Indian professional footballer who played as a defensive midfielder. During his career, Hossain has played for both of the Kolkata football giants, Mohun Bagan and East Bengal. He played with East Bengal for ten seasons, winning the Federation Cup three times. Also he played ISL on behalf of Kerala Blusters for 3 times. He also managed to gain 31 caps for the India national football team between 2005 and 2014, scoring twice for the country. Also he played ISL on behalf of Kerala Blasters for 3 times.";
+}
+
+var btn = document.getElementById("popup-close");
+btn.onclick = function() {
+    document.getElementById("popup").style.display = "none";
+}
